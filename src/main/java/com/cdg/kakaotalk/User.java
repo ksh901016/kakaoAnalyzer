06cd4e947a03195890b80611dba289bc675a241f
KangSungHyun <ksh901016@naver.com>
2015-07-23 20:51:50 +0900

    대화내용 cotent user객체에 저장 시간 추출하여 peektime 구하는 메소드 구현

--- src/main/java/com/cdg/kakaotalk/User.java
+++ src/main/java/com/cdg/kakaotalk/User.java
@@ -1,17 +1,26 @@
 package com.cdg.kakaotalk;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
 	private String name;
-	public List<String> list;
-	
-	
+	private Map<String, Integer> contentList = new HashMap<String, Integer>();
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public void saveInform(String content) {
+		if (contentList.containsKey(content)) {
+			contentList.put(content, (contentList.get(content) + 1));
+		} else {
+			contentList.put(content, 0);
+		}
 	}
 
 }
